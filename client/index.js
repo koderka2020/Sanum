a9e02f26cb3b0ac0c8cf89adcad16001093d4b7d
Kim Chiu <kimchiux123@gmail.com>
2021-03-29 18:43:50 -0700

    removed components/App from index.js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
 
 // uncomment so that webpack can bundle styles
 import styles from './styles.scss';
