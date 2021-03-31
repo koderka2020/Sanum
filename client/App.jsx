910cf445fc13179b18193c41affc16b541e9e078
Kim Chiu <kimchiux123@gmail.com>
2021-03-30 20:52:47 -0700

    refactored redirect to work, added exact on / path

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component, setState, useContext } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+// import { render } from 'react-dom';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,12 +7,12 @@ import {
 } from 'react-router-dom';
 import Profile from './components/Profile';
 import Feed from './components/Feed';
-import FindFriends from './components/FindFriends';
+// import FindFriends from './components/FindFriends';
 import Login from './components/Login';
-import NavBar from './components/Navbar'; // does this need to be within switch or outside  of it
-import FriendProfile from './components/FriendProfile';
-import FeedItems from './components/FeedItems';
-import FeedPopUp from './components/FeedPopUp';
+// import NavBar from './components/Navbar'; // does this need to be within switch or outside  of it
+// import FriendProfile from './components/FriendProfile';
+// import FeedItems from './components/FeedItems';
+// import FeedPopUp from './components/FeedPopUp';
 import { store, StateProvider } from './store';
 
 
@@ -21,9 +21,9 @@ const App = (props) => {
     <StateProvider>
       <Router>
           <Switch>
-            <Route path="/" component={Login} />
+            <Route exact path="/" component={Login} />
             <Route path="/profile" component={Profile} />
-            <Route path="/feed" component={Feed} />
+            <Route path="/feed" component={Feed}/>
           </Switch>
       </Router>
     </StateProvider>
